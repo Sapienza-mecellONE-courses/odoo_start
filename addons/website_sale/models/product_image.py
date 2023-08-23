16995582cbd3150814e1f2cfe0f64ebe5a2c6841
Francescocyber <142528430+Francescocyber@users.noreply.github.com>
2023-08-23 22:15:16 +0200

    Revert "Update product_image.py"
    This reverts commit 2180282251cef44d58afad2d55e0199d16cb789a.

--- addons/website_sale/models/product_image.py
+++ addons/website_sale/models/product_image.py
@@ -14,7 +14,6 @@ class ProductImage(models.Model):
     _order = 'sequence, id'
 
     name = fields.Char("Name", required=True)
-    type = fields.Char("Type", required=True)
     sequence = fields.Integer(default=10, index=True)
 
     image_1920 = fields.Image(required=True)
